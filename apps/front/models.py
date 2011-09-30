aa40176c14a9095308797b20413d6230e8741eaa
Danilo Bargen <gezuru@gmail.com>
2011-10-01 00:53:06 +0200

    Lecturer photos

--- apps/front/models.py
+++ apps/front/models.py
@@ -1,5 +1,29 @@
+import os
+import random
+from django.conf import settings
 from django.db import models
 
 class Lecturer(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, primary_key=True)
     abbreviation = models.CharField(max_length=10, unique=True)
+    subjects = models.CharField(max_length=50, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    
+    def photo(self):
+        path = os.path.join(settings.MEDIA_ROOT, 'lecturers', '%s.jpg' % self.abbreviation.lower())
+        print path
+        if os.path.exists(path):
+            return settings.MEDIA_URL + 'lecturers/%s.jpg' % self.abbreviation.lower()
+        return None
+
+    def avg_rating_d(self):
+        return '%u.%u' % (random.randint(1, 5), random.randint(0, 9))
+
+    def avg_rating_m(self):
+        return '%u.%u' % (random.randint(1, 5), random.randint(0, 9))
+
+    def avg_rating_f(self):
+        return '%u.%u' % (random.randint(1, 5), random.randint(0, 9))
+
+    class Meta:
+        ordering = ['name']
