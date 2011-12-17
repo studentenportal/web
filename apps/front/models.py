81b8b7eb8ccbf14ced064f1eca229d9fda4caee7
Danilo Bargen <gezuru@gmail.com>
2011-12-17 02:34:02 +0100

    Document models

--- apps/front/models.py
+++ apps/front/models.py
@@ -2,15 +2,25 @@ import os
 import random
 from django.conf import settings
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
+
 
 class Lecturer(models.Model):
+    """A lecturer at HSR.
+
+    If there is a photo of that lecturer, it should go into the media folder
+    and the filename should be <abbreviation>.jpg.
+
+    """
     name = models.CharField(max_length=255, primary_key=True)
     abbreviation = models.CharField(max_length=10, unique=True)
     subjects = models.CharField(max_length=50, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
-    
+
     def photo(self):
-        path = os.path.join(settings.MEDIA_ROOT, 'lecturers', '%s.jpg' % self.abbreviation.lower())
+        path = os.path.join(settings.MEDIA_ROOT, 'lecturers',
+                '%s.jpg' % self.abbreviation.lower())
         if os.path.exists(path):
             return settings.MEDIA_URL + 'lecturers/%s.jpg' % self.abbreviation.lower()
         return None
@@ -32,3 +42,68 @@ class Lecturer(models.Model):
 
     class Meta:
         ordering = ['name']
+
+
+class DocumentCategory(models.Model):
+    """Categories (usually subjects) for the documents.
+
+    A document can have several categories.
+
+    """
+    name = models.CharField(max_length=32, unique=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Document(models.Model):
+    """A document that can be uploaded by students.
+
+    A document can have a description, categories, ratings, etc...
+
+    """
+    name = models.CharField(max_length=64, unique=True)
+    description = models.CharField(max_length=255, blank=True)
+    document = models.FileField(upload_to='documents')
+    uploader = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    upload_date = models.DateTimeField(auto_now_add=True)
+    categories = models.ManyToManyField(DocumentCategory)
+
+    def rating_exact(self):
+        """Return exact rating average."""
+        ratings = self.DocumentRating.values_list('rating', flat=True)
+        total = sum(ratings)
+        return float(total) / len(ratings)
+
+    def rating(self):
+        """Return rounded rating average."""
+        return int(round(self.rating_exact()))
+
+    def __unicode__(self):
+        return self.name
+
+
+class DocumentRating(models.Model):
+    """Rating for a document.
+
+    Valid values are integers between 1 and 5.
+
+    """
+    def validate_rating(value):
+        if value not in range(1, 6):
+            raise ValidationError(u'Rating must be between 1 and 5, not %s.' % value)
+
+    user = models.ForeignKey(User)
+    document = models.ForeignKey(Document, related_name='DocumentRating')
+    rating = models.PositiveSmallIntegerField(validators=[validate_rating])
+
+    # Custom model validation
+    def clean(self):
+        if self.user == self.document.uploader:
+            raise ValidationError(u'A user cannot rate his own uploads.')
+
+    def __unicode__(self):
+        return 'User %s Document %s Rating %u' % (self.user.username, self.document.name, self.rating)
+
+    class Meta:
+        unique_together = ('user', 'document')
--- apps/front/tests.py
+++ apps/front/tests.py
@@ -1,16 +1,27 @@
+# encoding=utf8
+from datetime import datetime
+
 from django.utils import unittest
 from django.test.client import Client
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError
 
-from apps.front.models import Lecturer
+from apps.front import models
 
 
-class LecturerTestCase(unittest.TestCase):
+### MODEL TESTS ###
+
+class LecturerModelTest(unittest.TestCase):
     def setUp(self):
-        self.lecturer = Lecturer.objects.create(
+        self.lecturer = models.Lecturer.objects.create(
                 name='Jussuf Jolder',
                 abbreviation='jol',
                 subjects='Testsubject',
                 description='Some guy')
+
+    def tearDown(self):
+        self.lecturer.delete()
 
     def testValidRatingsRange(self):
         """Rating must be between 1 and 6."""
@@ -19,46 +30,102 @@ class LecturerTestCase(unittest.TestCase):
         self.assertTrue(1.0 <= self.lecturer.avg_rating_f() <= 6.0)
 
 
-class HomeViewTestCase(unittest.TestCase):
+class DocumentModelTest(unittest.TestCase):
     def setUp(self):
-        self.c = Client()
+        self.john = User.objects.create_user('john', 'john@example.com', 'johnpasswd')
+        self.marc = User.objects.create_user('marc', 'marc@example.com', 'marcpasswd')
+        self.pete = User.objects.create_user('pete', 'pete@example.com', 'petepasswd')
+        self.document = models.Document.objects.create(
+                name='Analysis 1 Theoriesammlung',
+                description='Dieses Dokument ist eine Zusammenfassung der \
+                    Theorie aus dem AnI1-Skript auf 8 Seiten. Das Dokument ist \
+                    in LaTeX gesetzt, Source ist hier: http://j.mp/fjtleh - \
+                    Gute Ergänzungen sind erwünscht!',
+                uploader=self.john)
+        self.document.DocumentRating.create(user=self.marc, rating=5)
+        self.document.DocumentRating.create(user=self.pete, rating=2)
+
+    def tearDown(self):
+        # Remove all created objects
+        self.john.delete()
+        self.marc.delete()
+        self.pete.delete()
+        self.document.delete()
+
+    def testBasicProperties(self):
+        self.assertEqual(self.document.name, 'Analysis 1 Theoriesammlung')
+        self.assertTrue(self.document.description.startswith('Dieses Dokument'))
+        self.assertTrue(isinstance(self.document.uploader, User))
+
+    def testUploadDate(self):
+        """Check whether upload date has been set."""
+        self.assertTrue(isinstance(self.document.upload_date, datetime))
+
+    def testRatingAverage(self):
+        self.assertEqual(self.document.DocumentRating.count(), 2)
+        self.assertEqual(self.document.rating(), 4)
+        self.assertEqual(self.document.rating_exact(), 3.5)
+
+    def testRatingValidation(self):
+        dr = models.DocumentRating.objects.get(document=self.document, user=self.marc)
+        dr.rating = 6
+        self.assertRaises(ValidationError, dr.full_clean)
+        dr.rating = 0
+        self.assertRaises(ValidationError, dr.full_clean)
+
+    def testRatingAuthorValidation(self):
+        """A user may not rate his own uploads."""
+        dr = models.DocumentRating(document=self.document, user=self.john)
+        self.assertRaises(ValidationError, dr.full_clean)
+
+    def testDuplicateRatingsValidation(self):
+        """A user cannot rate the same document twice."""
+        dr = models.DocumentRating(document=self.document, user=self.marc)
+        self.assertRaises(IntegrityError, dr.save)
+
+
+### VIEW TESTS ###
+
+class HomeViewTest(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
 
     def testHTTP200(self):
-        response = self.c.get('/')
+        response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
 
-class LecturersViewTestCase(unittest.TestCase):
+class LecturersViewTest(unittest.TestCase):
     def setUp(self):
-        self.c = Client()
+        self.client = Client()
 
     def testHTTP200(self):
-        response = self.c.get('/dozenten/')
+        response = self.client.get('/dozenten/')
         self.assertEqual(response.status_code, 200)
 
     def testTitle(self):
-        response = self.c.get('/dozenten/')
+        response = self.client.get('/dozenten/')
         self.assertIn('<h2>Unsere Dozenten</h2>', response.content)
 
 
-class ProfileViewTestCase(unittest.TestCase):
+class ProfileViewTest(unittest.TestCase):
     def setUp(self):
-        self.c = Client()
+        self.client = Client()
 
     def testUnauthRedirect(self):
-        response = self.c.get('/profil/')
+        response = self.client.get('/profil/')
         self.assertEqual(response.status_code, 302)
 
 
-class DocumentsViewTestCase(unittest.TestCase):
+class DocumentsViewTest(unittest.TestCase):
     def setUp(self):
-        self.c = Client()
+        self.client = Client()
         self.taburl = '/zusammenfassungen/'
 
     def testHTTP200(self):
-        response = self.c.get(self.taburl)
+        response = self.client.get(self.taburl)
         self.assertEqual(response.status_code, 200)
 
     def testTitle(self):
-        response = self.c.get(self.taburl)
+        response = self.client.get(self.taburl)
         self.assertIn('<h2>Zusammenfassungen</h2>', response.content)
