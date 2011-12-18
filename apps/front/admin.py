cf9a403f1b6d094ffa781d035d8509a0708f6474
Danilo Bargen <gezuru@gmail.com>
2011-12-18 02:03:50 +0100

    admin.py

---
+++ apps/front/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from apps.front import models
+
+admin.site.register(models.Lecturer)
+admin.site.register(models.Document)
+admin.site.register(models.DocumentCategory)
+admin.site.register(models.DocumentRating)
