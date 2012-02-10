3a9b2444e025a18af7d74bbabcc4628be06b1835
Danilo Bargen <gezuru@gmail.com>
2012-02-10 02:31:30 +0100

    Disabled csslint reports (useless!)

--- settings.py
+++ settings.py
@@ -176,7 +176,7 @@ JENKINS_TASKS = (
     'django_jenkins.tasks.run_pep8',
     'django_jenkins.tasks.run_pyflakes',
     'django_jenkins.tasks.run_jslint',
-    'django_jenkins.tasks.run_csslint',
+    #'django_jenkins.tasks.run_csslint',
     'django_jenkins.tasks.run_sloccount',
 )
 
