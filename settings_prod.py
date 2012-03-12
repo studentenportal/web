895ca84c99f4f680b086ffd8ea9fabf85d5260a5
Danilo Bargen <gezuru@gmail.com>
2012-03-13 00:12:19 +0100

    Sendfile configuration for Nginx (refs #24)

--- settings_prod.py
+++ settings_prod.py
@@ -3,4 +3,6 @@ from settings import *
 DEBUG = False
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 SENDFILE_BACKEND = 'sendfile.backends.nginx'
+SENDFILE_ROOT = os.path.join(MEDIA_ROOT, 'documents')
+SENDFILE_URL = MEDIA_URL + 'documents/'
 INSTALLED_APPS += ('gunicorn',)
