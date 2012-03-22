2496f51110ec34663a70edd0513e94075500c6ca
Danilo Bargen <gezuru@gmail.com>
2012-03-22 23:22:26 +0100

    Fabfile for deployment

---
+++ fabfile.py
@@ -0,0 +1,24 @@
+from fabric.api import settings, abort, local, cd, sudo, env
+from fabric.contrib.console import confirm
+
+env.hosts = ['dbargen@studentenportal.ch']
+
+def test():
+    """Run django tests."""
+    with settings(warn_only=True):
+        result = local('./manage.py test front')
+    if result.failed and not confirm("Tests failed. Continue anyway?"):
+        abort("Aborting at user request.")
+
+def prepare_deploy():
+    """Do everything needed before deployment."""
+    test()
+
+def deploy():
+    """Prepare & run deployment."""
+    prepare_deploy()
+    code_dir = '/var/www/studentenportal'
+    with cd(code_dir):
+        sudo('git pull', user='django')
+        sudo('VIRTUAL/bin/python manage.py collectstatic --noinput --settings=settings_prod', user='django')
+        sudo('/etc/init.d/supervisor restart')
