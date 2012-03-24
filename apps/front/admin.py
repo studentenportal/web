d842f180bebad6affe051dd8649ac72827709697
Danilo Bargen <gezuru@gmail.com>
2012-03-24 17:31:26 +0100

    Show id and description on documentcategory django-admin page

--- apps/front/admin.py
+++ apps/front/admin.py
@@ -4,10 +4,13 @@ from apps.front import models
 class QuoteAdmin(admin.ModelAdmin):
     list_filter = ('author', 'lecturer')
 
+class DocumentCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id', 'description')
+
 admin.site.register(models.Lecturer)
 admin.site.register(models.LecturerRating)
 admin.site.register(models.Quote, QuoteAdmin)
 admin.site.register(models.Document)
-admin.site.register(models.DocumentCategory)
+admin.site.register(models.DocumentCategory, DocumentCategoryAdmin)
 admin.site.register(models.DocumentRating)
 admin.site.register(models.Event)
