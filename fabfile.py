88a6dcb3bfe9e841aa5f3763c77619faef99ac05
Danilo Bargen <gezuru@gmail.com>
2012-03-24 19:25:19 +0100

    Updated fabfile

--- fabfile.py
+++ fabfile.py
@@ -15,9 +15,8 @@ def push():
     test()
     local('git push')
 
-def deploy():
-    """Prepare & run deployment."""
-    test()
+def deploy_untested():
+    """Prepare & run deployment without testing."""
     code_dir = '/var/www/studentenportal'
     with cd(code_dir):
         sudo('git pull', user='django')
@@ -25,3 +24,8 @@ def deploy():
         sudo('VIRTUAL/bin/pip install -r requirements_prod.txt', user='django')
         sudo('VIRTUAL/bin/python manage.py collectstatic --noinput --clear --settings=settings_prod', user='django')
         sudo('/etc/init.d/supervisor restart')
+
+def deploy():
+    """Prepare, test & run deployment."""
+    test()
+    deploy_untested()
