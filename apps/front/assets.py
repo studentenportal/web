a3c29d985ed56245fc48c6ec579647651edbd413
Danilo Bargen <gezuru@gmail.com>
2012-06-22 00:25:18 +0200

    Reordered assets.py

--- apps/front/assets.py
+++ apps/front/assets.py
@@ -1,13 +1,20 @@
 from django_assets import Bundle, register
 
+
 scripts = Bundle(
     'js/jquery-1.7.1.js',
-    'js/studentenportal.js',
     'js/bootstrap.js',
     'js/jquery.raty.min.js',
+    'js/studentenportal.js',
     'js/raty.js',
-    #filters='jsmin',
     output='js/packed.js')
+
+
+flattr_js = Bundle(
+    'js/flattr_loader.js',
+    filters='jsmin',
+    output='js/flattr_loader.min.js')
+
 
 stylesheets = Bundle(
     'css/style.css',
@@ -16,10 +23,6 @@ stylesheets = Bundle(
     #filters='cssmin,cssrewrite',
     output='css/packed.css')
 
-flattr_js = Bundle(
-    'js/flattr_loader.js',
-    filters='jsmin',
-    output='js/flattr_loader.min.js')
 
 register('scripts', scripts)
 register('stylesheets', stylesheets)
