28490e9b7481da4b14638953582e9110a4bbaca8
Danilo Bargen <gezuru@gmail.com>
2013-01-11 00:11:37 +0100

    Convert file extension of downloads to lowercase before checking it.

--- apps/front/views.py
+++ apps/front/views.py
@@ -328,7 +328,7 @@ class DocumentDownload(View):
             models.DocumentDownload.objects.create(document=doc, ip=ip)
         # Serve file
         filename = unicodedata.normalize('NFKD', doc.original_filename).encode('us-ascii', 'ignore')
-        attachment = not filename.endswith('.pdf')
+        attachment = not filename.lower().endswith('.pdf')
         return sendfile(request, doc.document.path,
                 attachment=attachment, attachment_filename=filename)
 
