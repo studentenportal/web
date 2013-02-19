88dee189bcd84e87f96a3a44358d14bf127ab623
Danilo Bargen <gezuru@gmail.com>
2013-02-19 22:33:22 +0100

    Django 1.4.4 security update

--- settings_base.py
+++ settings_base.py
@@ -26,6 +26,8 @@ LANGUAGE_CODE = 'de-ch'
 
 SITE_ID = 1
 
+ALLOWED_HOSTS = ['.studentenportal.ch']
+
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
 USE_I18N = True
