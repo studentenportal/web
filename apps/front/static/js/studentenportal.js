04d6160bbe2b945bd12b41a4958327ef0717c561
Danilo Bargen <gezuru@gmail.com>
2013-03-27 20:43:28 +0100

    Better raven error handling

--- apps/front/static/js/studentenportal.js
+++ apps/front/static/js/studentenportal.js
@@ -1,23 +1,3 @@
-// Error handling stuff
-
-var raven_options = {
-    logger: 'javascript',
-    ignoreUrls: [],
-    ignoreErrors: [],
-    includePaths: [/https?:\/\/(www\.)?studentenportal\.ch/]
-};
-Raven.config('http://cbaa9dff64764fee963e7f71a0d7a898@sentry.studentenportal.ch/2', raven_options).install()
-
-Dajaxice.setup({
-    'default_exception_callback': function() {
-        alert('Sorry, ein Fehler ist aufgetreten. Er wurde geloggt und ein Admin wurde benachrichtigt.');
-        Raven.captureMessage('Unknown Dajaxice error', {tags: { key: "value" }})
-    }
-});
-
-
-// Main stuff
-
 $(document).ready(function() {
     // Enable dropdowns
     $('.dropdown-toggle').dropdown()
@@ -29,4 +9,11 @@ $(document).ready(function() {
     };
     $('#id_start_date').datepicker(options);
     $('#id_end_date').datepicker(options);
+});
+
+Dajaxice.setup({
+    'default_exception_callback': function() {
+        alert('Sorry, ein Fehler ist aufgetreten. Er wurde geloggt und ein Admin wurde benachrichtigt.');
+        Raven.captureMessage('Unknown Dajaxice error on page ' + window.location.pathname)
+    }
 });
