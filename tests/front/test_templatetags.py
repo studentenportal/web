e3d5741a98e180d482b43d7742f30431924579c0
Danilo Bargen <mail@dbrgn.ch>
2014-02-08 14:50:48 +0100

    Moved tests into separate directory

--- tests/front/test_templatetags.py
+++ tests/front/test_templatetags.py
@@ -1,23 +1,22 @@
 # -*- coding: utf-8 -*-
 from django.test import SimpleTestCase
-from django.contrib.auth import get_user_model
 
-from apps.front import templatetags
+from apps.front.templatetags import tags
 
 
 ### TEMPLATETAG TESTS ###
 
 class GetRangeTest(SimpleTestCase):
     def testZero(self):
-        r = templatetags.tags.get_range(0)
+        r = tags.get_range(0)
         self.assertEqual(len(r), 0)
 
     def testNegative(self):
-        r = templatetags.tags.get_range(-5)
+        r = tags.get_range(-5)
         self.assertEqual(len(r), 0)
 
     def testPositive(self):
-        r = templatetags.tags.get_range(5)
+        r = tags.get_range(5)
         self.assertEqual(len(r), 5)
         self.assertEqual(r[0], 0)
         self.assertEqual(r[4], 4)
@@ -25,20 +24,20 @@ class GetRangeTest(SimpleTestCase):
 
 class GetRange1Test(SimpleTestCase):
     def testZero(self):
-        r = templatetags.tags.get_range1(0)
+        r = tags.get_range1(0)
         self.assertEqual(len(r), 0)
 
     def testOne(self):
-        r = templatetags.tags.get_range1(1)
+        r = tags.get_range1(1)
         self.assertEqual(len(r), 1)
         self.assertEqual(r[0], 1)
 
     def testNegative(self):
-        r = templatetags.tags.get_range1(-5)
+        r = tags.get_range1(-5)
         self.assertEqual(len(r), 0)
 
     def testPositive(self):
-        r = templatetags.tags.get_range1(5)
+        r = tags.get_range1(5)
         self.assertEqual(len(r), 5)
         self.assertEqual(r[0], 1)
         self.assertEqual(r[4], 5)
