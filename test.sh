11db66c3a318c03df6d793c16afd8faaad39d6df
Danilo Bargen <mail@dbrgn.ch>
2014-02-13 22:48:27 +0100

    Testing configuration

--- test.sh
+++ test.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+export TWITTER_CONSUMER_KEY=bHCF7StTA5E6qpbkNdOAA
+export TWITTER_CONSUMER_SECRET=dqZg18xISSXfqHXKCuDnlj83DcH8OfrO1vO8kCT2sus
+export TWITTER_ACCESS_KEY=505685594-GT9JYo0DQYTowjiFpviX0iGtqTc5qxkgtvoFT5pV
+export TWITTER_ACCESS_SECRET=ORiGqX32m8KoSGTW8kSWz9CeW3YU3BbvUfbkXc51J700t
+
 coverage run --source apps -m py.test \
 && echo \
 && coverage report
--- tests/tweets/test_views.py
+++ tests/tweets/test_views.py
@@ -17,7 +17,9 @@ class TweetListTest(TestCase):
     Author = namedtuple('User', ['name', 'screen_name'])
 
     tweet1 = Tweet(id=431356619243266048,
-                   text='"GeoDesign@HSR - why #Geodesign?" talk by Prof Carl Steinitz, Harvard Univ., 19. Mai 2014, 17h, Aula #HSR Hochschule für Technik Rapperswil',
+                   text='"GeoDesign@HSR - why #Geodesign?" talk by Prof Carl Steinitz, ' +
+                        'Harvard Univ., 19. Mai 2014, 17h, Aula #HSR Hochschule für ' +
+                        'Technik Rapperswil',
                    created_at=datetime.now() - timedelta(hours=9),
                    author=Author(name='Stefan Keller', screen_name='sfkeller'))
 
@@ -27,15 +29,15 @@ class TweetListTest(TestCase):
                    author=Author(name='Michael Weibel', screen_name='weibelm'))
 
     tweet3 = Tweet(id=391948408346255360,
-                   text='Ladezeit der Modul-Liste (https://studentenportal.ch/dokumente/ ) von 3.8s auf 0.7s verringert :) #sql #optimierungen',
+                   text='Ladezeit der Modul-Liste (https://studentenportal.ch/dokumente/ ) ' +
+                        'von 3.8s auf 0.7s verringert :) #sql #optimierungen',
                    created_at=datetime.now(),
                    author=Author(name='VSHSRstudentenportal ', screen_name='studportal_hsr'))
 
     @patch.object(tweepy.API, 'search')
     @patch.object(tweepy.API, 'user_timeline')
-    def testTweetsFromApiAreDisplayed(self, tweepy_search, tweepy_user_timeline):
+    def test_tweet_display(self, tweepy_search, tweepy_user_timeline):
         #Arrange
-        dir(self.tweet1)
         tweepy_search.return_value = [self.tweet1, self.tweet2]
         tweepy_user_timeline.return_value = [self.tweet3]
 
@@ -43,6 +45,6 @@ class TweetListTest(TestCase):
         response = self.client.get(reverse('tweets:tweet_list'))
 
         #Assert
-        self.assertEqual(len(response.context['tweets']), 3)
-        self.assertContains(response, '@weibelm')
-        self.assertContains(response, '@sfkeller')
+        assert len(response.context['tweets']) == 3
+        self.assertContains('@weibelm', response)
+        self.assertContains('@sfkeller', response)
