3f9946812f0d941605bc14bb9be78e050094f19c
Lukas Martinelli <me@lukasmartinelli.ch>
2014-04-17 20:35:27 +0200

    Fixed PEP8 issues that made build fail.

--- apps/front/management/commands/fetch_blacklist.py
+++ apps/front/management/commands/fetch_blacklist.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from __future__ import print_function, division, absolute_import, unicode_literals
+
 import sys
 import ldap
 from optparse import make_option
@@ -9,10 +11,8 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     help = '[NOT FINISHED] Fetch list of lecturers, add them to a blacklist.'
     option_list = BaseCommand.option_list + (
-        make_option('--user',
-            dest='username', help='HSR username'),
-        make_option('--pass',
-            dest='password', help='HSR password'),
+        make_option('--user', dest='username', help='HSR username'),
+        make_option('--pass', dest='password', help='HSR password'),
     )
 
     def printO(self, msg, newline=True):
@@ -34,27 +34,27 @@ class Command(BaseCommand):
         assert options['username'], '--user argument required'
         assert 'password' in options, '--pass argument required'
         assert options['password'], '--pass argument required'
-        #pw = getpass.getpass() # TODO
+        # pw = getpass.getpass()  # TODO
         pw = options['password']
 
         dn = 'CN=%s,OU=Stud,OU=HSR,OU=FH_Users,DC=hsr,DC=ch' % options['username']
         con = ldap.initialize('ldap://hsr.ch:389')
-        #con.protocol_version = ldap.VERSION3
+        # con.protocol_version = ldap.VERSION3
         con.simple_bind_s(dn, pw)
 
-        print 'bound'
+        print('bound')
 
-        #basedn_stud = 'OU=Stud,OU=HSR,OU=FH_Users,DC=hsr,DC=ch'
+        # basedn_stud = 'OU=Stud,OU=HSR,OU=FH_Users,DC=hsr,DC=ch'
         basedn_pers = 'OU=Pers,OU=HSR,OU=FH_Users,DC=hsr,DC=ch'
 
-        #fltr = ''
-        #attrs = ['cn']
+        # fltr = ''
+        # attrs = ['cn']
 
         result = con.search_s(basedn_pers, ldap.SCOPE_SUBTREE, '(objectClass=*)', ['cn', 'mail'])
 
-        print 'gotten result'
+        print('gotten result')
 
         from pprint import pprint
         pprint(result)
-        #result_data = con.result(result_id, 0)
-        #print result_data
+        # result_data = con.result(result_id, 0)
+        # print result_data
