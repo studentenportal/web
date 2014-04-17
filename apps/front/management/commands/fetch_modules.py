f1d88161a1a1ac52c3192bfbf683eb43b0407702
Lukas Martinelli <me@lukasmartinelli.ch>
2014-04-17 17:18:01 +0200

    Refactored fetch_modules #22
    Refactored fetch_modules #22

--- apps/front/management/commands/fetch_modules.py
+++ apps/front/management/commands/fetch_modules.py
@@ -20,28 +20,66 @@ blacklist = ['3D-Vis', 'DigT1', 'DigT2', 'MaTechM1', 'MaTechM2', 'ElMasch', 'StR
 class Command(CommandOutputMixin, NoArgsCommand):
     help = 'Fetch module descriptions and write them to the database.'
 
-    def parse_module_description(self, url):
-        """Parse a module detail page to extract the required ects points,
-        the described objectives, the main lecturer and the associated courses"""
-        r = requests.get(url)
-        soup = BeautifulSoup(r.content)
-        table = soup.find('tbody', id=re.compile('^modul'))
+    def parse_module_detail_page(self, url):
+        """
+        Parse a module detail page and return it's extracted properties.
+        """
+        try:
+            r = requests.get(url)
+            soup = BeautifulSoup(r.content)
 
-        ects_points = int(table.find('tr', id=re.compile('^Kreditpunkte')).find_all('td')[1].text)
-        objectives = table.find('tr', id=re.compile('^Lernziele')).find_all('td')[1].string
-        lecturer = table.find('tr', id=re.compile('^dozent')).find_all('td')[1].text
+            def get_course(row):
+                return re.match('(.*)\(', row.find('strong').text).group(1).strip()
 
-        relation_table = soup.find('tbody', id=re.compile('^kategorieZuordnungen'))
-        relation_rows = relation_table.find_all('div', {'class':'katZuordnung'})
+            table = soup.find('tbody', id=re.compile('^modul'))
+            course_table = soup.find('tbody', id=re.compile('^kategorieZuordnungen'))
+            course_rows = course_table.find_all('div', {'class': 'katZuordnung'})
 
-        def get_course(row):
-            return re.match('(.*)\(', row.find('strong').text).group(1).strip()
+            return {
+                'ects_points': int(table.find('tr', id=re.compile('^Kreditpunkte')).find_all('td')[1].text),
+                'objectives': table.find('tr', id=re.compile('^Lernziele')).find_all('td')[1].string,
+                'lecturer': table.find('tr', id=re.compile('^dozent')).find_all('td')[1].text,
+                'courses': {get_course(row) for row in course_rows}
+            }
+        except:
+            self.stderr.write("Could not parse {0}: {1}", url, sys.exc_info()[0])
 
-        courses = {get_course(row) for row in relation_rows}
+    def parse_modules(self, url):
+        """
+        Parse all modules in the table and their corresponding detail page.
+        Returns a dictionary containing all extracted data.
+        """
+        try:
+            r = requests.get(url)
+            soup = BeautifulSoup(r.content)
+            table = soup.find('table')
+            rows = table.find_all('tr', recursive=False)
 
-        return (ects_points, objectives, lecturer, relation_rows)
+            for row in rows[1:]: #Skip heading row
+                cols = row.find_all('td', recursive=False)
 
+                yield {
+                    "url": cols[0].a['href'],
+                    "description": cols[0].text,
+                    "full_name": cols[1].text,
+                    "name": cols[1].text.split('_', 1)[1],
+                    "dates": cols[2].text,
+                    "detail": self.parse_module_detail_page(cols[0].a['href'])
+                }
+        except:
+            self.stderr.write("Could not parse {0}: {1}", url, sys.exc_info()[0])
 
+    def create_document_category(self, module):
+        """
+        Create a DocumentCategory in the database if it does not yet exist.
+        Returns True if a DocumentCategory was created.
+        """
+        try:
+            DocumentCategory.objects.get(name=module["name"])
+            return False
+        except DocumentCategory.DoesNotExist:
+            DocumentCategory.objects.create(name=module["name"], description=module["description"])
+            return True
 
     def handle_noargs(self, **options):
         # Initialize counters
@@ -50,63 +88,41 @@ class Command(CommandOutputMixin, NoArgsCommand):
         invalid_count = 0
         blacklisted_count = 0
         added_count = 0
-        previous_module_name = ""
 
-        r = requests.get('http://studien.hsr.ch/')
-        soup = BeautifulSoup(r.content)
-        table = soup.find('table')
-        rows = table.find_all('tr', recursive=False)
-        for row in rows:
-            cols = row.find_all('td', recursive=False)
+        for module in self.parse_modules('http://studien.hsr.ch/'):
+            # Skip conditions
+            if module["name"] in blacklist:
+                self.stdout.write('Skipping %s (blacklisted)' % module["name"])
+                blacklisted_count += 1
+                continue
+            if not module["full_name"].startswith('M_'):
+                self.stdout.write('Skipping %s (invalid module name)' % module["name"])
+                invalid_count += 1
+                continue
+            if 'nicht durchgeführt' in module["dates"]:
+                self.stdout.write('Skipping %s (no valid dates)' % module["name"])
+                invalid_count += 1
+                continue
+            if module["description"].startswith('Seminar - ') or \
+                    module["description"].startswith('Bachelor-Arbeit ') or \
+                    module["description"].startswith('Studienarbeit ') or \
+                    module["description"].startswith('Projektarbeit ') or \
+                    module["description"].startswith('Diplomarbeit '):
+                self.stdout.write('Skipping %s (project or seminary)' % module["name"])
+                invalid_count += 1
+                continue
 
-            if len(cols):  # If this is not a header row...
+            parsed_count += 1
 
-                # Parse data
-                url = cols[0].a['href']
-                description = cols[0].text
-                full_name = cols[1].text
-                name = full_name.split('_', 1)[1]
-                dates = cols[2].text
+            if self.create_document_category(module):
+                self.stdout.write('Added %s' % module["name"])
+                added_count += 1
+            else:
+                self.stdout.write(u'Skipping %s (already exists)' % module["name"])
+                existing_count += 1
 
-                # Skip conditions
-                if name in blacklist:
-                    self.printO('Skipping %s (blacklisted)' % name)
-                    blacklisted_count += 1
-                    continue
-                if not full_name.startswith('M_'):
-                    self.printO('Skipping %s (invalid module name)' % name)
-                    invalid_count += 1
-                    continue
-                if 'nicht durchgeführt' in dates:
-                    self.printO('Skipping %s (no valid dates)' % name)
-                    invalid_count += 1
-                    continue
-                if description.startswith('Seminar - ') or \
-                        description.startswith('Bachelor-Arbeit ') or \
-                        description.startswith('Studienarbeit ') or \
-                        description.startswith('Projektarbeit ') or \
-                        description.startswith('Diplomarbeit '):
-                    self.printO('Skipping %s (project or seminary)' % name)
-                    invalid_count += 1
-                    continue
-
-                parsed_count += 1
-
-                ects_points, objectives, lecturer, relation_rows = self.parse_module_description(url)
-
-                # Create module if new
-                try:
-                    DocumentCategory.objects.get(name=name)
-                except DocumentCategory.DoesNotExist:
-                    self.printO('Adding %s' % name)
-                    DocumentCategory.objects.create(name=name, description=description)
-                    added_count += 1
-                else:
-                    self.printO(u'Skipping %s (already exists)' % name)
-                    existing_count += 1
-
-        self.printO(u'\nParsed %u modules.' % parsed_count)
-        self.printO(u'Added %u modules.' % added_count)
-        self.printO(u'Skipped %u modules (already exist).' % existing_count)
-        self.printO(u'Skipped %u modules (blacklist).' % blacklisted_count)
-        self.printO(u'Skipped %u modules (invalid).' % invalid_count)
+        self.stdout.write(u'\nParsed %u modules.' % parsed_count)
+        self.stdout.write(u'Added %u modules.' % added_count)
+        self.stdout.write(u'Skipped %u modules (already exist).' % existing_count)
+        self.stdout.write(u'Skipped %u modules (blacklist).' % blacklisted_count)
+        self.stdout.write(u'Skipped %u modules (invalid).' % invalid_count)
