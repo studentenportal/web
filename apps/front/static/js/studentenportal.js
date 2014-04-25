63018f52cf19ce80eb3d7bbe4e1375fc910fc572
Lukas Martinelli <me@lukasmartinelli.ch>
2014-04-25 17:13:10 +0200

    Removed JQuery wherever possible!

--- apps/front/static/js/studentenportal.js
+++ apps/front/static/js/studentenportal.js
@@ -1,23 +1,11 @@
-$(document).ready(function() {
+(function() {
     var navigation = responsiveNav(".nav-collapse", {
         customToggle: "#toggle"
     });
-
-    // Enable dropdowns
-    $('.dropdown-toggle').dropdown()
-
-    // Enable datepickers
-    options = {
-        dateFormat: 'dd.mm.yy',
-        showAnim: 'fadeIn',
-    };
-    $('#id_start_date').datepicker(options);
-    $('#id_end_date').datepicker(options);
-});
-
-Dajaxice.setup({
-    'default_exception_callback': function() {
-        alert('Sorry, ein Fehler ist aufgetreten. Er wurde geloggt und ein Admin wurde benachrichtigt.');
-        Raven.captureMessage('Unknown Dajaxice error on page ' + window.location.pathname)
-    }
-});
+    Dajaxice.setup({
+        'default_exception_callback': function() {
+            alert('Sorry, ein Fehler ist aufgetreten. Er wurde geloggt und ein Admin wurde benachrichtigt.');
+            Raven.captureMessage('Unknown Dajaxice error on page ' + window.location.pathname)
+        }
+    });
+})();
