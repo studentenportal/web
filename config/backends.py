f5c1a2af148c3136359553abb5d2074f4110accc
Lukas Martinelli <me@lukasmartinelli.ch>
2014-12-31 10:29:33 +0100

    Fixed issue with mail login (fixes #169)

--- config/backends.py
+++ config/backends.py
@@ -2,6 +2,16 @@ from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
 
 
+def strip_mail_part(username):
+    """
+    Users might try to login with their email. To support that
+    we can simply strip the mail part from the username
+    """
+    if '@' in username:
+        return username.split('@')[0]
+    return username
+
+
 class CaseInsensitiveModelBackend(ModelBackend):
     """
     By default ModelBackend does case _sensitive_ username authentication, which isn't what is
@@ -11,6 +21,7 @@ class CaseInsensitiveModelBackend(ModelBackend):
 
     """
     def authenticate(self, username=None, password=None):
+        username = strip_mail_part(username)
         User = get_user_model()
         try:
             user = User.objects.get(username__iexact=username)
