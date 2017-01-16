e9d8fe320e4295f03ab4b87dc454154f1c36b085
Raphael Zimmermann <mail@raphael.li>
2017-01-16 21:34:38 +0100

    Wait for postgres in tests

--- test.sh
+++ test.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Wait for postgre
+while ! curl http://$DB_HOST:5432/ 2>&1 | grep '52' > /dev/null
+do
+  sleep 1
+done
+
 export TWITTER_CONSUMER_KEY=bHCF7StTA5E6qpbkNdOAA
 export TWITTER_CONSUMER_SECRET=dqZg18xISSXfqHXKCuDnlj83DcH8OfrO1vO8kCT2sus
 export TWITTER_ACCESS_KEY=505685594-GT9JYo0DQYTowjiFpviX0iGtqTc5qxkgtvoFT5pV
