8622cf6c8b35c8d4bb696468e29c8075c77c4e25
Fabian Hauser <fabian@fh2.ch>
2018-06-21 16:28:42 +0200

    Build script/production Dockerfile corrections

--- deploy/production/travis-build.bash
+++ deploy/production/travis-build.bash
@@ -2,7 +2,7 @@
 # Expects DOCKER_TAGS to be set.
 set -ex
 
-docker build --file=./deploy/production/Dockerfile studentenportal/studentenportal
+docker build --file=./deploy/production/Dockerfile --tag=studentenportal/studentenportal .
 
 for tag in $DOCKER_TAGS; do
     docker tag studentenportal/studentenportal $tag
