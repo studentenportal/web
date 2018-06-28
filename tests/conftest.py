c76e545e306e2e45d589d7e1d61efe7dffb1a02e
Florian Bruhin <git@the-compiler.org>
2018-06-28 19:29:12 +0200

    Use fixtures for user management

---
+++ tests/conftest.py
@@ -0,0 +1,18 @@
+import pytest
+
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()   # FIXME use fixture?
+
+
+@pytest.fixture
+def user(db):
+    return User.objects.create_user(username='testuser', password='test',
+                                    email='test@studentenportal.ch')
+
+
+@pytest.fixture
+def auth_client(client, user):
+    client.login(username='testuser', password='test')
+    return client
