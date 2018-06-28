7f3dca952059e78fd2ab8f43231435f28f30a97d
Florian Bruhin <git@the-compiler.org>
2018-06-28 21:49:11 +0200

    Rewrite lecturers.test_ajax

--- tests/lecturers/test_ajax.py
+++ tests/lecturers/test_ajax.py
@@ -4,7 +4,7 @@ from __future__ import print_function, division, absolute_import, unicode_litera
 import json
 import urllib
 
-from django.test import TestCase
+import pytest
 
 from model_mommy import mommy
 
@@ -12,37 +12,39 @@ from apps.lecturers import models
 from apps.front import models as front_models
 
 
-class VoteQuoteTest(TestCase):
+class TestVoteQuote:
+
     """Test whether adding, changing or removing a vote via AJAX works."""
 
-    def sendRequest(self, payload):
+    def send_request(self, client, payload):
         """Send a dajaxice request with the specified payload. Return response."""
         url = '/dajaxice/apps.lecturers.vote_quote/'
         data = {'argv': json.dumps(payload)}
-        return self.client.post(url,
+        return client.post(url,
             data=urllib.urlencode(data),
             content_type='application/x-www-form-urlencoded',
             HTTP_X_REQUESTED_WITH='XMLHttpRequest')
 
-    def testVoting(self):
+    @pytest.mark.django_db
+    def test_voting(self, client):
         quote = mommy.make(models.Quote)
         front_models.User.objects.create_user('fakename', 'fake@example.com', 'fakepwd')
 
-        self.client.login(username='fakename', password='fakepwd')
+        client.login(username='fakename', password='fakepwd')
 
-        response = self.sendRequest({'vote': 'down', 'quote_pk': quote.pk})
+        response = self.send_request(client, {'vote': 'down', 'quote_pk': quote.pk})
         assert response.status_code == 200
         assert response.content != 'DAJAXICE_EXCEPTION', 'Dajaxice exception occured.'
         assert models.QuoteVote.objects.count() == 1
         assert quote.vote_sum() == -1
 
-        response = self.sendRequest({'vote': 'up', 'quote_pk': quote.pk})
+        response = self.send_request(client, {'vote': 'up', 'quote_pk': quote.pk})
         assert response.status_code == 200
         assert response.content != 'DAJAXICE_EXCEPTION', 'Dajaxice exception occured.'
         assert models.QuoteVote.objects.count() == 1
         assert quote.vote_sum() == 1
 
-        response = self.sendRequest({'vote': 'remove', 'quote_pk': quote.pk})
+        response = self.send_request(client, {'vote': 'remove', 'quote_pk': quote.pk})
         assert response.status_code == 200
         assert response.content != 'DAJAXICE_EXCEPTION', 'Dajaxice exception occured.'
         assert models.QuoteVote.objects.count() == 0
