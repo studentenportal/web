4354264c00f94e69def022e43742167d347af7fb
Florian Bruhin <git@the-compiler.org>
2018-06-28 22:13:13 +0200

    Rewrite lectures.test_models

--- tests/lecturers/test_models.py
+++ tests/lecturers/test_models.py
@@ -3,7 +3,6 @@ from __future__ import print_function, division, absolute_import, unicode_litera
 
 import datetime
 
-from django.test import TestCase
 from django.core.exceptions import ValidationError
 from django.contrib.auth import get_user_model
 from django.db import IntegrityError
@@ -17,26 +16,30 @@ import pytest
 User = get_user_model()
 
 
-class LecturerModelTest(TestCase):
-    def setUp(self):
-        self.lecturer = mommy.make(models.Lecturer, title='Prof. Dr.',
+class TestLecturerModel:
+
+    @pytest.fixture
+    def lecturer(self, db):
+        lecturer = mommy.make(models.Lecturer, title='Prof. Dr.',
             first_name='David',
             last_name='Krakaduku',
         )
-        mommy.make(models.LecturerRating, lecturer=self.lecturer, category='d', rating=5)
-        mommy.make(models.LecturerRating, lecturer=self.lecturer, category='d', rating=8)
-        mommy.make(models.LecturerRating, lecturer=self.lecturer, category='m', rating=10)
+        mommy.make(models.LecturerRating, lecturer=lecturer, category='d', rating=5)
+        mommy.make(models.LecturerRating, lecturer=lecturer, category='d', rating=8)
+        mommy.make(models.LecturerRating, lecturer=lecturer, category='m', rating=10)
+        return lecturer
 
-    def testRatings(self):
+    def test_ratings(self, lecturer):
         """Test whether ratings are calculated correctly."""
-        assert self.lecturer.avg_rating_d() == 7
-        assert self.lecturer.avg_rating_m() == 10
-        assert self.lecturer.avg_rating_f() == 0
+        assert lecturer.avg_rating_d() == 7
+        assert lecturer.avg_rating_m() == 10
+        assert lecturer.avg_rating_f() == 0
 
-    def testName(self):
-        assert self.lecturer.name() == 'Prof. Dr. Krakaduku David'
+    def test_name(self, lecturer):
+        assert lecturer.name() == 'Prof. Dr. Krakaduku David'
 
-    def testManager(self):
+    @pytest.mark.django_db
+    def test_manager(self):
         mommy.make(models.Lecturer, pk=10, function='Dozent')
         mommy.make(models.Lecturer, pk=11, department='Gebäudemanagement')
         mommy.make(models.Lecturer, pk=12, function='Projektmitarbeiterin')
@@ -50,9 +53,10 @@ class LecturerModelTest(TestCase):
         assert 12 not in real_lecturers.values_list('pk', flat=True)
 
 
-class LecturerRatingModelTest(TestCase):
-    def setUp(self):
-        # setUpClass
+class TestLecturerRatingModel:
+
+    @pytest.fixture(autouse=True)
+    def create_objects(self, db):
         mommy.make(User)
         mommy.make(models.Lecturer)
 
@@ -68,37 +72,39 @@ class LecturerRatingModelTest(TestCase):
         lr.full_clean()
         lr.save()
 
-    def testAddSimpleRating(self):
+    def test_add_simple_rating(self):
         """Test whether a simple valid rating can be added."""
         self.create_default_rating()
         assert models.LecturerRating.objects.count() == 1
 
-    def testAddInvalidCategory(self):
+    def test_add_invalid_category(self):
         """Test whether only a valid category can be added."""
         with pytest.raises(ValidationError):
             self.create_default_rating(category=u'x')
 
-    def testAddInvalidRating(self):
+    def test_add_invalid_rating(self):
         """Test whether invalid ratings raise validation errors."""
         with pytest.raises(ValidationError):
             self.create_default_rating(rating=11)
         with pytest.raises(ValidationError):
             self.create_default_rating(rating=0)
 
-    def testUniqueTogether(self):
+    def test_unique_together(self):
         """Test the unique_together constraint."""
         self.create_default_rating()
         with pytest.raises(ValidationError):
             self.create_default_rating(rating=6)
 
 
-class QuoteModelTest(TestCase):
-    def testQuote(self):
-        quote = "Dies ist ein längeres Zitat, das dazu dient, zu testen " + \
-            "ob man Zitate erfassen kann und ob die Länge des Zitats mehr " + \
-            "als 255 Zeichen enthalten darf. Damit kann man sicherstellen, " + \
-            "dass im Model kein CharField verwendet wurde. Denn wir wollen " + \
-            "ja nicht, dass längere Zitate hier keinen Platz haben :)"
+class TestQuoteModel:
+
+    @pytest.mark.django_db
+    def test_quote(self):
+        quote = ("Dies ist ein längeres Zitat, das dazu dient, zu testen "
+                 "ob man Zitate erfassen kann und ob die Länge des Zitats mehr "
+                 "als 255 Zeichen enthalten darf. Damit kann man sicherstellen, "
+                 "dass im Model kein CharField verwendet wurde. Denn wir wollen "
+                 "ja nicht, dass längere Zitate hier keinen Platz haben :)")
         before = datetime.datetime.now()
         q = models.Quote()
         q.author = mommy.make(User)
@@ -110,7 +116,8 @@ class QuoteModelTest(TestCase):
         assert before < q.date < after
         assert q.date_available()
 
-    def testNullValueAuthor(self):
+    @pytest.mark.django_db
+    def test_null_value_author(self):
         q = models.Quote()
         q.lecturer = mommy.make(models.Lecturer)
         q.quote = 'spam'
@@ -118,9 +125,10 @@ class QuoteModelTest(TestCase):
         try:
             q.save()
         except IntegrityError:
-            self.fail("A quote with no author should not throw an IntegrityError.")
+            pytest.fail("A quote with no author should not throw an IntegrityError.")
 
-    def test1970Quote(self):
+    @pytest.mark.django_db
+    def test_1970_quote(self):
         """Check whether a quote from 1970-1-1 is marked as date_available=False."""
         q = models.Quote()
         q.lecturer = mommy.make(models.Lecturer)
