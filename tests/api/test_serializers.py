6e724fe5f6dd9653275917ea29fe4ca9df99f699
Florian Bruhin <git@the-compiler.org>
2018-06-28 18:34:13 +0200

    Rewrite api.test_serializers

--- tests/api/test_serializers.py
+++ tests/api/test_serializers.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function, division, absolute_import, unicode_literals
 
-from django.test import TestCase
+import pytest
 from django.core.urlresolvers import reverse
 from django.contrib.auth import get_user_model
 
@@ -10,20 +10,19 @@ from model_mommy import mommy
 from apps.api import serializers
 
 
-class UserSerializerTest(TestCase):
-
-    def testSerialization(self):
-        """A simple serialization test case."""
-        user = mommy.make(get_user_model(), flattr='flttr', twitter='twttr')
-        serializer = serializers.UserSerializer(user)
-        data = serializer.data
-        url = reverse('api:user_detail', args=(user.pk,))
-        assert data == {
-            'url': url,
-            'username': user.username,
-            'first_name': user.first_name,
-            'last_name': user.last_name,
-            'email': user.email,
-            'flattr': 'flttr',
-            'twitter': 'twttr'
-        }
+@pytest.mark.django_db
+def test_serialization():
+    """A simple serialization test case."""
+    user = mommy.make(get_user_model(), flattr='flttr', twitter='twttr')
+    serializer = serializers.UserSerializer(user)
+    data = serializer.data
+    url = reverse('api:user_detail', args=(user.pk,))
+    assert data == {
+        'url': url,
+        'username': user.username,
+        'first_name': user.first_name,
+        'last_name': user.last_name,
+        'email': user.email,
+        'flattr': 'flttr',
+        'twitter': 'twttr'
+    }
