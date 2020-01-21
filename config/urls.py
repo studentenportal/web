bc5404bd3720fcf819aa7ea2ce613f504269153c
Florian Bruhin <me@the-compiler.org>
2020-01-21 09:31:50 +0100

    Fix django-debug-toolbar
    See https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#setting-up-urlconf

--- config/urls.py
+++ config/urls.py
@@ -45,3 +45,9 @@ if settings.DEBUG:
         url(r'static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
         url(r'media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     ]
+
+if settings.DEBUG_TOOLBAR:
+    import debug_toolbar
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
