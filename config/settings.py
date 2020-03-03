cbd8c3a574ef8c80b9cad99735130c85a5abae6c
Florian Bruhin <me@the-compiler.org>
2020-03-03 12:33:49 +0100

    Make it possible to set mail host/user/password

--- config/settings.py
+++ config/settings.py
@@ -263,6 +263,11 @@ if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    if 'DJANGO_EMAIL_HOST' in os.environ:
+        EMAIL_HOST = env('DJANGO_EMAIL_HOST')
+        EMAIL_HOST_USER = env('DJANGO_EMAIL_HOST_USER')
+        EMAIL_HOST_PASSWORD = env('DJANGO_EMAIL_HOST_PASSWORD')
+        EMAIL_USE_TLS = True
 
 # Sendfile
 if DEBUG:
