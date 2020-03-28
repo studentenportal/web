841bdfd2db05b9451983a7ed16407346815aa160
Danilo Bargen <mail@dbrgn.ch>
2020-03-28 21:53:37 +0100

    CI: Publish Docker images from master (#254)

--- deploy/production/ci-publish.sh
+++ deploy/production/ci-publish.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Expects DOCKER_PASSWORD, DOCKER_USERNAME and DOCKER_TAGS to be set.
-set -ex
+set -euxo pipefail
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 for tag in $DOCKER_TAGS; do
-    docker push $tag
+    docker push "$tag"
 done
