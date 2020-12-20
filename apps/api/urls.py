cf63a32ad4862d8c5aa14094d57a065de17130e0
murthy10 <samuel.kurath@gmail.com>
2020-12-20 17:34:57 +0100

    refactored depricated url with re_path

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import include, url
+from django.urls import include, re_path
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from apps.api import views
@@ -8,26 +8,26 @@ app_name = "api"
 
 
 v1_api = [
-    url(r"^$", views.api_root, name="api_root"),
-    url(r"^users$", views.UserList.as_view(), name="user_list"),
-    url(r"^users/(?P<pk>-?\d+)$", views.UserDetail.as_view(), name="user_detail"),
-    url(r"^lecturers$", views.LecturerList.as_view(), name="lecturer_list"),
-    url(
+    re_path(r"^$", views.api_root, name="api_root"),
+    re_path(r"^users$", views.UserList.as_view(), name="user_list"),
+    re_path(r"^users/(?P<pk>-?\d+)$", views.UserDetail.as_view(), name="user_detail"),
+    re_path(r"^lecturers$", views.LecturerList.as_view(), name="lecturer_list"),
+    re_path(
         r"^lecturers/(?P<pk>-?\d+)$",
         views.LecturerDetail.as_view(),
         name="lecturer_detail",
     ),
-    url(
+    re_path(
         r"^lecturers/(?P<pk>-?\d+)/rate$",
         views.LecturerRate.as_view(),
         name="lecturer_rate",
     ),
-    url(r"^quotes$", views.QuoteList.as_view(), name="quote_list"),
-    url(r"^quotes/(?P<pk>-?\d+)$", views.QuoteDetail.as_view(), name="quote_detail"),
-    url(r"^quotes/(?P<pk>-?\d+)/vote$", views.QuoteVote.as_view(), name="quote_vote"),
+    re_path(r"^quotes$", views.QuoteList.as_view(), name="quote_list"),
+    re_path(r"^quotes/(?P<pk>-?\d+)$", views.QuoteDetail.as_view(), name="quote_detail"),
+    re_path(r"^quotes/(?P<pk>-?\d+)/vote$", views.QuoteVote.as_view(), name="quote_vote"),
 ]
 
 urlpatterns = [
-    url(r"^", include("rest_framework.urls")),
-    url(r"^v1/", include(format_suffix_patterns(v1_api))),
+    re_path(r"^", include("rest_framework.urls")),
+    re_path(r"^v1/", include(format_suffix_patterns(v1_api))),
 ]
