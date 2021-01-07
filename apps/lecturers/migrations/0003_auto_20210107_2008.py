f7bf94e91e125498ab682bc91755b92b7e5828b9
murthy10 <samuel.kurath@gmail.com>
2021-01-07 20:14:57 +0100

    formatted lecturer migration

--- apps/lecturers/migrations/0003_auto_20210107_2008.py
+++ apps/lecturers/migrations/0003_auto_20210107_2008.py
@@ -7,18 +7,25 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('lecturers', '0002_auto_20201130_1115'),
+        ("lecturers", "0002_auto_20201130_1115"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='lecturer',
-            name='picture',
-            field=models.ImageField(blank=True, null=True, upload_to=apps.lecturers.models.lecturer_directory_path, verbose_name='Bild'),
+            model_name="lecturer",
+            name="picture",
+            field=models.ImageField(
+                blank=True,
+                null=True,
+                upload_to=apps.lecturers.models.lecturer_directory_path,
+                verbose_name="Bild",
+            ),
         ),
         migrations.AlterField(
-            model_name='lecturer',
-            name='id',
-            field=models.AutoField(primary_key=True, serialize=False, verbose_name='ID'),
+            model_name="lecturer",
+            name="id",
+            field=models.AutoField(
+                primary_key=True, serialize=False, verbose_name="ID"
+            ),
         ),
     ]
