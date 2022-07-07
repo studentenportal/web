042f64cfb9dcf1f872a8040be0393db2e90a0afa
Florian Bruhin <me@the-compiler.org>
2022-07-07 10:18:16 +0200

    ci: Also build nginx image
    See https://github.com/studentenportal/deploy/issues/32

--- deploy/production/ci-build.sh
+++ deploy/production/ci-build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-# Expects DOCKER_TAGS to be set.
+# Expects DOCKER_TAGS and DOCKER_TAGS_NGINX to be set.
 set -euxo pipefail
 
 docker build --file=./deploy/production/Dockerfile --tag=studentenportal/web .
 for tag in $DOCKER_TAGS; do
     docker tag studentenportal/web "$tag"
+done
+
+docker build --file=./deploy/production/nginx/Dockerfile --tag=studentenportal/nginx .
+for tag in $DOCKER_TAGS_NGINX; do
+    docker tag studentenportal/nginx "$tag"
 done
--- deploy/production/ci-publish.sh
+++ deploy/production/ci-publish.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Expects DOCKER_PASSWORD, DOCKER_USERNAME and DOCKER_TAGS to be set.
+# Expects DOCKER_PASSWORD, DOCKER_USERNAME, DOCKER_TAGS and DOCKER_TAGS_NGINX to be set.
 set -euxo pipefail
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-for tag in $DOCKER_TAGS; do
+for tag in $DOCKER_TAGS $DOCKER_TAGS_NGINX; do
     docker push "$tag"
 done
